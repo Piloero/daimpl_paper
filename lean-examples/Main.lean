bcf5743f76e54efe8027784ccf96e3e038bfc902
Jonas Jahnel <jonas.jahnel@stud.tu-darmstadt.de>
2025-02-02 23:55:05 +0100

    art

--- lean-examples/Main.lean
+++ lean-examples/Main.lean
@@ -1,4 +1,43 @@
-import LeanExamples
+def listing1 (x y: Nat) : Nat :=
+  let z := x + y
 
+  if x > 4 then
+    /- return -/ x + y
+  else
+    /- return -/ z + 2
+
+def listing1_elim (x y: Nat) : Nat :=
+  let h := x + y
+  let z := h
+
+  if x > 4 then
+    /- return -/ h
+  else
+    /- return -/ z + 2
+
+def listing2 (x y : Nat) : (Nat × Nat) :=
+  let a := x + y
+  let b := x + y
+  (a, b)
+
+def listing2_elim (x y : Nat) : (Nat × Nat) :=
+  let a := x + y
+  let b := a
+  (a, b)
+
+
+-- you can play around with the numbers and see that
+-- regardles of input paramers the output will be the same
+def x := 10
+def y := 10
+
+#eval listing1 x y == listing1_elim x y
+#eval listing2 x y == listing2_elim x y
+
+#eval listing1 x y
+
+-- you don't have to use this
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  do
+    IO.println s!"listing1: {listing1 x y}"
+    IO.println s!"listing1_elim {listing1_elim x y}"
